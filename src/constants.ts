1d2d5ae4d57623972d38b0ae6d92833c48146fbb
g111348015-cloud <g111348015@grad.ntue.edu.tw>
2026-04-30 15:20:05 +0800

    更新 src/constants.ts via EZPage

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const SUGAR_LEVELS = ['正常糖', '七分糖', '五分糖', '三分糖', '微糖', '無糖'];
+export const ICE_LEVELS = ['正常冰', '少冰', '微冰', '去冰', '溫', '熱'];
+
+export const INITIAL_CATEGORIES = [
+  { name: '醇茶', englishName: 'PURE TEA', order: 1 },
+  { name: '奶茶', englishName: 'MILK TEA', order: 2 },
+  { name: '鮮萃', englishName: 'FRESH FRUIT', order: 3 },
+];
+
+export const INITIAL_PRODUCTS = [
+  { name: '經典五桐茶', englishName: 'Wootea', categoryName: '醇茶', priceM: 30, priceL: 35, isPopular: true, isHot: true },
+  { name: '杏仁凍五桐茶', englishName: 'Wootea with Almond Jelly', categoryName: '醇茶', priceL: 49, isPopular: true, isHot: false },
+  { name: '玉堂春茶王', englishName: 'Gardenia Tea King', categoryName: '醇茶', priceM: 40, priceL: 45, isPopular: true, isHot: true },
+  { name: '老實人紅茶', englishName: 'Black Tea', categoryName: '醇茶', priceM: 30, priceL: 35, isPopular: false, isHot: true },
+  { name: '一把青茶', englishName: 'Turquoise Tea', categoryName: '醇茶', priceM: 30, priceL: 35, isPopular: false, isHot: true },
+  { name: '茉莉香片', englishName: 'Green Tea', categoryName: '醇茶', priceM: 30, priceL: 35, isPopular: false, isHot: true },
+  { name: '清香烏龍', englishName: 'Oolong Tea', categoryName: '醇茶', priceM: 30, priceL: 35, isPopular: false, isHot: true },
+];
