3cbbbd5806844205aa49d4f0bc07368661348993
g111348015-cloud <g111348015@grad.ntue.edu.tw>
2026-04-30 15:20:06 +0800

    更新 src/lib/utils.ts via EZPage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,11 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function formatPrice(price: number) {
-  return new Intl.NumberFormat('zh-TW', {
-    style: 'currency',
-    currency: 'TWD',
-    minimumFractionDigits: 0,
-  }).format(price);
-}
